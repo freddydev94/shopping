bd8dd528fc9b8d06d2e1538901bcf4a66460cba7
Freddy Hernandez <freddy.dev94@gmail.com>
2023-04-06 14:20:23 -0600

    Actualizacion

---
+++ Shopping/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Country> countries { get; set; }
+    }
+}
---
+++ Shopping/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name ="Pais")]
+        [Required (ErrorMessage ="El campo {0} es obligatorio" )]
+        [MaxLength(40, ErrorMessage ="El campo {} debe tener maximo {1} caracteres")]
+        public string Nombre { get; set; }
+    }
+}
